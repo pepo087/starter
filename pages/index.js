982964fb6135aaa5da9b467da880e4739a8390e2
pompeo t <48530804+pepo087@users.noreply.github.com>
2024-03-05 17:16:04 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,6 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-         <iframe style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="640" height="480" src="https://charts.mongodb.com/charts-project-0-oecfb/embed/charts?id=65e203af-0286-44f7-8a51-145838d646a6&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
         </p>
       </main>
 
