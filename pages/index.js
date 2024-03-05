2798a222067b4a84fc1c5335a7d5977c129bdcd6
pompeo t <48530804+pepo087@users.noreply.github.com>
2024-03-05 17:24:49 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        {/* The iframe element should be placed above the Header component */}
-        <iframe
-          style="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);"
-          width="640"
-          height="480"
-          src="https://charts.mongodb.com/charts-project-0-oecfb/embed/charts?id=65e203af-0286-44f7-8a51-145838d646a6&maxDataAge=3600&theme=light&autoRefresh=true"
-        />
+        
 
         <Header title="Welcome to my app!" />
         <p className="description">
